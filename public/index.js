d5457a62871f5131bd45fd0a191a52e2327893cb
Sarah <sarahd.rios@gmail.comm>
2020-01-14 23:22:56 -0600

    code jquery api call

--- public/index.js
+++ public/index.js
@@ -1,22 +1,23 @@
-$(document).ready(()=>{
+$(document).ready(() => {
 
-let nasaURL = 'https://api.nasa.gov/planetary/apod?api_key=gAV3SkyoF0XO00UHGXcOn32RjLQehbeuBqBUo1jE&date='
+  let nasaURL = 'https://api.nasa.gov/planetary/apod?api_key=gAV3SkyoF0XO00UHGXcOn32RjLQehbeuBqBUo1jE&date='
 
-let userDate = $('#datepicker').val();
+  let userDate = $('#datepicker').val();
+  
+  //$('#datepicker').val(todayDate);
 
-$.ajax({
-  url: nasaURL + userDate,
+  //GET REQUEST to Nasa API 
+  $.ajax({
+    url: nasaURL + userDate,
     type: 'GET',
-  success: (data) => {
-    
-   
-$('#title').text(data.title);
-$('#date').text( data.date);
-$("#pic").attr('src',data.hdurl);
-$("#explanation").text(data.explanation);
-//$("#datepicker").attr('src',data.hdurl);
-  }
-});
+    success: (data) => {
+
+      $('#title').text(data.title);
+      $('#date').text(data.date);
+      $("#pic").attr('src', data.hdurl);
+      $("#explanation").text(data.explanation);
+    }
+  });
 
 
 });
