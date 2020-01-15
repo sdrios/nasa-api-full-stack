71aba723dcd8ce01e3b6c3977e22081371706a1f
Tanner Alsup <tanneralsup14@gmail.com>
2020-01-14 20:23:43 -0600

    take a look to see if you understand the issue with this code

--- public/index.js
+++ public/index.js
@@ -2,6 +2,13 @@ var req = new XMLHttpRequest();
 var url = "https://api.nasa.gov/planetary/apod?api_key=";
 var api_key = "RWjbf2gIl6znRHshrHKCPByMufezrNgVbnwrIfbu";
 
+var datePick = document.getElementById('datepicker');
+datePick.max = todaysDate();
+datePick.value = todaysDate();
+var date = datePick.value;
+
+var url = 'https://api.nasa.gov/planetary/apod?api_key=' + API_KEY + '&date=' + date;
+
 req.open("GET", url + api_key );
 req.send();
 
@@ -12,79 +19,7 @@ req.addEventListener("load", function(){
     document.getElementById("date").textContent = response.date;
     document.getElementById("pic").src = response.hdurl;
     document.getElementById("explanation").textContent = response.explanation;
+    document.getElementById("datepicker").src =response.hdurl;
   }
 })
 
-import { mdbDatePicker, mdbSpinner, mdbBtn } from "mdbvue";
-
-export default {
-  name: "Example",
-  components: {
-    mdbDatePicker,
-    mdbSpinner,
-    mdbBtn
-  },
-  methods: {
-    getPickerValue(pickerValue) {
-      var queryUrl = "https://api.nasa.gov/planetary/apod?";
-      var queryKey = "api_key=RWjbf2gIl6znRHshrHKCPByMufezrNgVbnwrIfbu";
-      var queryDate = "date=" + pickerValue + "&";
-      var queryFull = queryUrl + queryKey + queryDate;
-
-      axios
-        .get(queryFull)
-        .then(response => {
-          (this.date = response.data.date),
-            (this.explanation = response.data.explanation),
-            (this.media_type = response.data.media_type),
-            (this.title = response.data.title),
-            (this.url = response.data.url)((this.hdurl = response.data.hdurl));
-        })
-        .catch(error => {
-          console.log(error);
-          this.errored = true;
-        })
-        .finally(() => (this.loading = false));
-    }
-  },
-        
-      loading: true,
-      errored: false,
-      date: null,
-      explanation: null,
-      media_type: null,
-      title: null,
-      url: null,
-      hdurl: null,
-      selected: "",
-      today: null
-    };
-  
-
-  mounted() 
-    var queryUrl = "https://api.nasa.gov/planetary/apod?";
-    var queryKey = "api_key=RWjbf2gIl6znRHshrHKCPByMufezrNgVbnwrIfbu";
-    var queryDate = "";
-    var queryFull = queryUrl + queryKey + queryDate; 
-
-    var today = new Date();
-    var dd = String(today.getDate()).padStart(2, "0");
-    var mm = String(today.getMonth() + 1).padStart(2, "0"); 
-    var yyyy = today.getFullYear();
-
-    this.today = yyyy + "-" + mm + "-" + dd;
-
-    axios
-      .get(queryFull)
-      .then(response => {
-        (this.date = response.data.date),
-          (this.explanation = response.data.explanation),
-          (this.media_type = response.data.media_type),
-          (this.title = response.data.title),
-          (this.url = response.data.url)((this.hdurl = response.data.hdurl));
-      })
-      .catch(error => {
-        console.log(error);
-        this.errored = true;
-      })
-      .finally(() => (this.loading = false));
