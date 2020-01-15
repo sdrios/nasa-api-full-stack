7c31da431999d71bff7e27e5ef00b26282502b2c
Tanner Alsup <tanneralsup14@gmail.com>
2020-01-14 19:56:29 -0600

    trying to add button permitions

--- public/index.js
+++ public/index.js
@@ -15,4 +15,76 @@ req.addEventListener("load", function(){
   }
 })
 
+import { mdbDatePicker, mdbSpinner, mdbBtn } from "mdbvue";
 
+export default {
+  name: "Example",
+  components: {
+    mdbDatePicker,
+    mdbSpinner,
+    mdbBtn
+  },
+  methods: {
+    getPickerValue(pickerValue) {
+      var queryUrl = "https://api.nasa.gov/planetary/apod?";
+      var queryKey = "api_key=RWjbf2gIl6znRHshrHKCPByMufezrNgVbnwrIfbu";
+      var queryDate = "date=" + pickerValue + "&";
+      var queryFull = queryUrl + queryKey + queryDate;
+
+      axios
+        .get(queryFull)
+        .then(response => {
+          (this.date = response.data.date),
+            (this.explanation = response.data.explanation),
+            (this.media_type = response.data.media_type),
+            (this.title = response.data.title),
+            (this.url = response.data.url)((this.hdurl = response.data.hdurl));
+        })
+        .catch(error => {
+          console.log(error);
+          this.errored = true;
+        })
+        .finally(() => (this.loading = false));
+    }
+  },
+        
+      loading: true,
+      errored: false,
+      date: null,
+      explanation: null,
+      media_type: null,
+      title: null,
+      url: null,
+      hdurl: null,
+      selected: "",
+      today: null
+    };
+  
+
+  mounted() 
+    var queryUrl = "https://api.nasa.gov/planetary/apod?";
+    var queryKey = "api_key=RWjbf2gIl6znRHshrHKCPByMufezrNgVbnwrIfbu";
+    var queryDate = "";
+    var queryFull = queryUrl + queryKey + queryDate; 
+
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, "0");
+    var mm = String(today.getMonth() + 1).padStart(2, "0"); 
+    var yyyy = today.getFullYear();
+
+    this.today = yyyy + "-" + mm + "-" + dd;
+
+    axios
+      .get(queryFull)
+      .then(response => {
+        (this.date = response.data.date),
+          (this.explanation = response.data.explanation),
+          (this.media_type = response.data.media_type),
+          (this.title = response.data.title),
+          (this.url = response.data.url)((this.hdurl = response.data.hdurl));
+      })
+      .catch(error => {
+        console.log(error);
+        this.errored = true;
+      })
+      .finally(() => (this.loading = false));
